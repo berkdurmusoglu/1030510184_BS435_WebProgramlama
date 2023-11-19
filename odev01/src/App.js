c5b633e9dbff63bdb2e1a842bd2d60753625a05f
berkdurmusoglu <berkavnidurmusoglu@gmail.com>
2023-11-19 16:43:36 +0300

    Fix

--- odev01/src/App.js
+++ odev01/src/App.js
@@ -13,9 +13,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
+          
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React
         </a>
       </header>
     </div>
